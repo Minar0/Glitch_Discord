b0414da682b9d46ac3a1ef53d2a3f84fce3056a2
Minaro <38708197+Minar0@users.noreply.github.com>
2023-12-06 01:20:28 -0600

    Automated startup
    Using the wonders of batch scripting (and ChatGPT because I don't know batch scripting) I made this to automatically startup GlitchOS. Cast your eyes upon it and weep

---
+++ start.bat
@@ -0,0 +1,23 @@
+REM Nuru wanted to be in this comment.
+REM You'll have to change the location of Docker Desktop if you have it in a different location. Also this guy won't work at all  if you use Linux. Sorry.
+
+@echo off
+
+REM Start Docker engine
+echo Starting Docker engine...
+start "" "C:\Program Files\Docker\Docker\Docker Desktop.exe" --background
+
+REM Wait for Docker to start
+:CHECK_DOCKER
+timeout /t 2 /nobreak >nul
+docker info >nul 2>&1
+if errorlevel 1 goto CHECK_DOCKER
+
+echo Starting LlamaGPT webserver
+start "" /D "llama-gpt" run.sh --model 13b --with-cuda
+
+echo Starting GlitchOS...
+python bot.py
+
+echo Batch script completed.
+pause
